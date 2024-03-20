cf5764e5e6ca96b5dd86abd366bc376f438f37c4
Benjamin SA <benjamynfrank101@gmail.com>
2024-03-20 17:09:49 +0100

    initial commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./navbar.css";
+import logo from "../../assests/logo.png";
+
+const NavBar = () => {
+	return (
+		<nav className="navbar">
+			<img src="" alt="" />
+			<div className="desktopMenu">
+
+			</div>
+		</nav>
+	)
+}
+
+export default NavBar;
